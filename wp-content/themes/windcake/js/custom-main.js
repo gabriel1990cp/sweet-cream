55b3f61d37a4d564f56d4b17cba3c395ae7a41b3
jcarvalho89 <josiano.sc@hotmail.com>
2018-02-13 11:19:25 -0200

    ajsutes css e js

--- wp-content/themes/windcake/js/custom-main.js
+++ wp-content/themes/windcake/js/custom-main.js
@@ -1,50 +1,47 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+(function ($) {
+    'use strict';
+    jQuery(document).ready(function () {
 
+        /* 1. MENU JS */
 
- /* 1. MENU JS */
-
-/*
- * realiza scrollTo atraves de classes padrões
- */
-    jQuery('li.menu-item a,.mainmenu-area ul.navbar-nav li a,.logotipo').on('click', function (e) {
-        var anchor = jQuery(this);
-        scrolltop(anchor, e);
-    });
-
- /* realiza scrollTo atraves de classe especifica
- */
-    jQuery('.scroll-to').on('click', function (e) {
-        var anchor = jQuery(this);
-        scrolltopClasse(anchor, e);
-
-    });
-
-    function scrolltopClasse(anchor, e) {
-        jQuery('html, body').stop().animate({
-            scrollTop: jQuery(anchor.find('.target').data('url')).offset().top - 50
-        }, 1500);
-        e.preventDefault();
-    }
-
-    function scrolltop(anchor, e) {
-        jQuery('html, body').stop().animate({
-            scrollTop: jQuery(anchor.attr('href')).offset().top - 50
-        }, 1500);
-        e.preventDefault();
-    }
-
-    jQuery(window).on('scroll', function () {
-        if (jQuery(this).scrollTop() > 100) {
-            jQuery('.mainmenu-area').addClass('menu-animation');
-            jQuery('.header-3 .logo-area').addClass('menu-animation');
-            jQuery('.header-4 .logo-area').addClass('menu-animation');
-        } else {
-            jQuery('.mainmenu-area').removeClass('menu-animation');
-            jQuery('.header-3 .logo-area').removeClass('menu-animation');
-            jQuery('.header-4 .logo-area').removeClass('menu-animation');
+        /*
+         * realiza scrollTo atraves de classes padrões
+         */
+        jQuery('li.menu-item a,.mainmenu-area ul.navbar-nav li a,.logotipo').on('click', function (e) {
+            var anchor = jQuery(this);
+            scrolltop(anchor, e);
+        });
+        /* realiza scrollTo atraves de classe especifica
+         */
+        jQuery('.scroll-to').on('click', function (e) {
+            var anchor = jQuery(this);
+            scrolltopClasse(anchor, e);
+        });
+        function scrolltopClasse(anchor, e) {
+            jQuery('html, body').stop().animate({
+                scrollTop: jQuery(anchor.find('.target').data('url')).offset().top - 50
+            }, 1500);
+            e.preventDefault();
         }
+
+        function scrolltop(anchor, e) {
+            jQuery('html, body').stop().animate({
+                scrollTop: jQuery(anchor.attr('href')).offset().top - 50
+            }, 1500);
+            e.preventDefault();
+        }
+
+        jQuery(window).on('scroll', function () {
+            if (jQuery(this).scrollTop() > 100) {
+                jQuery('.mainmenu-area').addClass('menu-animation');
+                jQuery('.header-3 .logo-area').addClass('menu-animation');
+                jQuery('.header-4 .logo-area').addClass('menu-animation');
+            } else {
+                jQuery('.mainmenu-area').removeClass('menu-animation');
+                jQuery('.header-3 .logo-area').removeClass('menu-animation');
+                jQuery('.header-4 .logo-area').removeClass('menu-animation');
+            }
+        });
+
     });
+})(jQuery);
