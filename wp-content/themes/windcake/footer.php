5f182c121b9cfcfbc682beb24e632c11230827e9
Gabriel Costa Pinto <gabriel.cp1990@gmail.com>
2018-02-15 22:05:54 -0200

    Alterações CSS

--- wp-content/themes/windcake/footer.php
+++ wp-content/themes/windcake/footer.php
@@ -3,13 +3,7 @@
 		<div class="footer">
 			
 			<div class="container">
-				<div class="row-fluid footerblock">
-
-						<?php echo of_get_option('copyright'); ?>
-
-				</div>
-
-				<div class="row-fuid social_box">
+                <div class="row-fuid social_box">
 					<?php
 					if( of_get_option('facebook') !="") { ?>      
 					<a href="<?php echo of_get_option('facebook'); ?>" class="social" target="_blank"><i class="fa fa-facebook fa-lg"></i></a>
@@ -17,20 +11,23 @@
 					}
                     if( of_get_option('pinterest') !="") { ?>
 					<a href="<?php echo of_get_option('pinterest'); ?>" class="social" target="_blank"><i class="fa fa-pinterest fa-lg"></i></a>
-					<?php 
-					} 	
-					if( of_get_option('instagram') !="") { ?>  					
+					<?php
+					}
+                    if( of_get_option('twitter') !="") { ?>
+                        <a href="<?php echo of_get_option('twitter'); ?>" class="social" target="_blank"><i class="fa fa-twitter fa-lg"></i></a>
+                        <?php
+                    }
+                    if( of_get_option('instagram') !="") { ?>
 					<a href="<?php echo of_get_option('instagram'); ?>" class="social" target="_blank"><i class="fa fa-instagram fa-lg"></i></a>
 					<?php 
-					} 					
+					}
 					?>
-
-
-				</div>
-
-			</div>
-
-		</div>
+                </div>
+                <div class="row-fluid footerblock">
+                    <?php echo of_get_option('copyright'); ?>
+                </div>
+            </div>
+        </div>
 		
 		<!-- end footer block !-->
 										
